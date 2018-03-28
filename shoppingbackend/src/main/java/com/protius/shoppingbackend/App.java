2e4ce4966aceb9b0e513d0752870d52b66c56cd6
protius <protius@192.168.200.58>
2018-03-28 16:56:00 +0700

    test comit in github

---
+++ shoppingbackend/src/main/java/com/protius/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.protius.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
